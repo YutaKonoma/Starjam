7206e035f0f9c1dac58514dd20b62f6aab29e93c
sakamotosyo <syo10150000@gmail.com>
2022-07-24 12:39:26 +0900

    マップの生成処理を追加

---
+++ Assets/sakamoto/Script/SearchManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchManager : MonoBehaviour
+{
+    [Header("マップの高さ")]
+    [SerializeField] int _height = 18;
+    [Header("マップの横幅")]
+    [SerializeField] int _width = 18;
+
+    [Header("生成するオブジェクトのPrefab")]
+    [SerializeField] GameObject _mapPreab;
+
+    [Tooltip("マップデータの配列")]
+    BlockData[,] mapData;
+
+    void Start()
+    {
+        mapData = new BlockData[_height, _width];
+    }
+
+    /// <summary>
+    /// マップを生成する
+    /// </summary>
+    void CreateMap() 
+    {
+        for (int y = 0; y < mapData.Length / _width; y++) 
+        {
+            for (int x = 0; x < mapData.Length / _height; x++)
+            {
+                GameObject blockObj = Instantiate(_mapPreab, new Vector3(x, -1 * y, 0), _mapPreab.transform.rotation);
+            }
+        }
+    }
+}
+
+public struct BlockData
+{
+    public BlockData(Color color) 
+    {
+        Color MyColor = color;
+    }
+
+}
